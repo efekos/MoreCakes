555b4b9f0f52fa01c0077940b7ea55899f79f64b
efekos <hefeolgun10@gmail.com>
2024-02-17 17:24:58 +0300

    add chocolate bucket items

--- src/main/java/dev/efekos/morecakes/registry/MoreCakesItems.java
+++ src/main/java/dev/efekos/morecakes/registry/MoreCakesItems.java
@@ -1,13 +1,20 @@
 package dev.efekos.morecakes.registry;
 
 import dev.efekos.morecakes.MoreCakes;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 
 public class MoreCakesItems {
     private static <T extends Item> T register(String id,T item){
         return Registry.register(Registries.ITEM,new Identifier(MoreCakes.MOD_ID,id),item);
     }
+
+    public static final Item COCOA_BEANS_BUCKET = register("cocoa_beans_bucket",new Item(new FabricItemSettings().maxCount(1).recipeRemainder(Items.BUCKET).rarity(Rarity.COMMON)));
+    public static final Item COCOA_BUCKET = register("cocoa_bucket",new Item(new FabricItemSettings().maxCount(1).recipeRemainder(Items.BUCKET).rarity(Rarity.COMMON)));
+    public static final Item CHOCOLATE_BUCKET = register("chocolate_bucket",new Item(new FabricItemSettings().maxCount(1).recipeRemainder(Items.BUCKET).rarity(Rarity.COMMON)));
 }
