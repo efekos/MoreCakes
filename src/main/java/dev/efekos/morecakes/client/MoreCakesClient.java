28896d62361c6e6e41211905b13187df4807e6ee
efekos <hefeolgun10@gmail.com>
2024-02-19 22:27:45 +0300

    fix model overrides

--- src/main/java/dev/efekos/morecakes/client/MoreCakesClient.java
+++ src/main/java/dev/efekos/morecakes/client/MoreCakesClient.java
@@ -5,21 +5,20 @@ import dev.efekos.morecakes.registry.MoreCakesBlocks;
 import dev.efekos.morecakes.registry.MoreCakesItems;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.item.ModelPredicateProvider;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 
 public class MoreCakesClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlock(MoreCakesBlocks.BLUE_BERRY_BUSH, RenderLayer.getCutout());
-
-        ModelPredicateProviderRegistry.register(MoreCakesItems.HUGE_CAKE,new Identifier(MoreCakes.MOD_ID,"type"),(stack, world, entity, seed) -> {
-            NbtCompound nbt = stack.getOrCreateNbt();
-            if(nbt.contains("Type", NbtElement.INT_TYPE)) return nbt.getInt("Type");
-            else return 0;
-        });
     }
 }
--- src/main/java/dev/efekos/morecakes/registry/MoreCakesItems.java
+++ src/main/java/dev/efekos/morecakes/registry/MoreCakesItems.java
@@ -20,6 +20,6 @@ public class MoreCakesItems {
     public static final Item COCOA_BEANS_BUCKET = register("cocoa_beans_bucket",new Item(new FabricItemSettings().maxCount(1).recipeRemainder(Items.BUCKET).rarity(Rarity.COMMON)));
     public static final Item COCOA_BUCKET = register("cocoa_bucket",new Item(new FabricItemSettings().maxCount(1).recipeRemainder(Items.BUCKET).rarity(Rarity.COMMON)));
     public static final Item CHOCOLATE_BUCKET = register("chocolate_bucket",new Item(new FabricItemSettings().maxCount(1).recipeRemainder(Items.BUCKET).rarity(Rarity.COMMON)));
-    public static final Item HUGE_CAKE = register("huge_cake",new HugeCakeItem(new FabricItemSettings().maxCount(1)));
+    public static final HugeCakeItem HUGE_CAKE = register("huge_cake",new HugeCakeItem(new FabricItemSettings().maxCount(1)));
     public static final AliasedBlockItem BLUE_BERRIES = register("blue_berries",new AliasedBlockItem(MoreCakesBlocks.BLUE_BERRY_BUSH,new FabricItemSettings()));
 }
