5f25c1f455e6268a006c3df70c216f58abb22255
efekos <hefeolgun10@gmail.com>
2024-02-19 22:27:52 +0300

    add huge cakes to group

--- src/main/java/dev/efekos/morecakes/registry/MoreCakesGroups.java
+++ src/main/java/dev/efekos/morecakes/registry/MoreCakesGroups.java
@@ -1,7 +1,9 @@
 package dev.efekos.morecakes.registry;
 
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.text.Text;
 
 public class MoreCakesGroups {
@@ -18,6 +20,15 @@ public class MoreCakesGroups {
                 entries.add(MoreCakesBlocks.CHOCOLATE_CAKE_SWEETBERRY, ItemGroup.StackVisibility.PARENT_AND_SEARCH_TABS);
                 entries.add(MoreCakesBlocks.CAKE_CHOCOLATE_BLUE_BERRY, ItemGroup.StackVisibility.PARENT_AND_SEARCH_TABS);
                 entries.add(MoreCakesBlocks.CHOCOLATE_CAKE_GLOWBERRY, ItemGroup.StackVisibility.PARENT_AND_SEARCH_TABS);
+
+                for (int i = 0; i < 512; i++) {
+                    ItemStack stack = MoreCakesItems.HUGE_CAKE.getDefaultStack();
+                    NbtCompound nbt = stack.getNbt();
+                    nbt.putInt("Type",i);
+                    stack.setNbt(nbt);
+                    entries.add(stack);
+                }
+
                 entries.add(MoreCakesItems.COCOA_BEANS_BUCKET, ItemGroup.StackVisibility.SEARCH_TAB_ONLY);
                 entries.add(MoreCakesItems.COCOA_BUCKET,ItemGroup.StackVisibility.SEARCH_TAB_ONLY);
                 entries.add(MoreCakesItems.CHOCOLATE_BUCKET,ItemGroup.StackVisibility.SEARCH_TAB_ONLY);
